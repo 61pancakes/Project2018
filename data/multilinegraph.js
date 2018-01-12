62237cab70c49e25fb62550bc23257b26dc9e97f
61pancakes <61pancakes@users.noreply.github.com>
2018-01-12 13:08:25 +0100

    Add files via upload

---
+++ data/multilinegraph.js
@@ -0,0 +1,80 @@
+d3.json("http://localhost:8000/studenten.json", function (error, data) {
+
+    var margin = { top: 20, right: 50, bottom: 30, left: 40 },
+        width = 500 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom,
+        years = 6;
+
+    var y = d3.scale.linear()
+        .domain([2000, 0])
+        .range([0, height]);
+
+    var x = d3.scale.ordinal() // Jaren op de x-as 
+        .domain(d3.range(years))
+        .rangeBands([0, width], .5);
+
+    var xLabels = d3.scale.ordinal() // Jaren op de x-as 
+        .domain(['\'12-\'13', '\'13-\'14', '\'14-\'15', '\'15-\'16', '\'16-\'17', '\'17-\'18',])
+        .rangeBands([0, width], .5);
+
+    // Create the svg.
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("svg:g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Show the axes.
+    var xAxis = d3.svg.axis()
+        .scale(xLabels)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var yAxisRight = d3.svg.axis()
+        .scale(y)
+        .orient("right");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + width + ",0)")
+        .call(yAxisRight);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Transform data from string to int in multidimensional array.
+    var finalData = [];
+    var jaren = ["twaalfdertien", "dertienveertien", "veertienvijftien", "vijftienzestien", "zestienzeventien", "zeventienachttien"];
+
+    for (var i = 0; i < 4; i++) {
+        finalData[i] = [6];
+        for (var j = 0; j < 6; j++) {
+            finalData[i][j] = parseInt(data.students[i][jaren[j]]);
+        }
+    }
+
+    console.log(finalData);
+
+    // create a line function that can convert data[] into x and y points
+    var line = d3.svg.line()
+        .x(function (d, i) { return x(i); })
+        .y(function (d) { return y(d); })
+
+    svg.append("path")		// Add the valueline path.
+        .attr("class", "line")
+        .attr("d", line(finalData[0]));
+
+    svg.append("path")		// Add the valueline2 path.
+        .attr("class", "line")
+        .style("stroke", "red")
+        .attr("d", line(finalData[1]));
+})
