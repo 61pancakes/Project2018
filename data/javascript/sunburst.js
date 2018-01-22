b46b33171562e064c3ae1a7961fc8f868faa4606
61pancakes <andrea@wcw-stud-145-109-14-5.wireless.uva.nl>
2018-01-22 14:56:05 +0100

    asddfghj

--- data/javascript/sunburst.js
+++ data/javascript/sunburst.js
@@ -66,6 +66,7 @@
 
     /* Load dataset from local server to create the circle partitions. */
     d3.json("data/json/sunburst.json", function (error, root) {
+        console.log("TEST!!!");
         var formatNumber = d3.format(",d");
 
         svg.selectAll("path")
