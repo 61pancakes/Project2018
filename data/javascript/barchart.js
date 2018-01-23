49fff59ec448a51c4d8044ffe62d5fe7d29f7127
61pancakes <andrea@wcw-stud-145-109-58-209.wireless.uva.nl>
2018-01-23 14:52:42 +0100

    interactivity erop of eronder

--- data/javascript/barchart.js
+++ data/javascript/barchart.js
@@ -200,6 +200,7 @@ function createBarchart() {
             console.log(this.id);
 
             test(d);
+            gotoyear(this.id);
             // Access sunburstdata.
             // Zoom in this.id.
         };
--- data/javascript/sunburst.js
+++ data/javascript/sunburst.js
@@ -107,6 +107,24 @@ function click(d) {
         .attrTween("d", function (d) { return function () { return arc(d); }; });
 }
 
+
+function gotoyear(year) {
+    d = data.filter(
+        function (data) { return data.name == year }
+    );
+
+    svg.transition()
+        .duration(750)
+        .tween("scale", function () {
+            var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+                yd = d3.interpolate(y.domain(), [d.y, 1]),
+                yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+            return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+        })
+        .selectAll("path")
+        .attrTween("d", function (d) { return function () { return arc(d); }; });
+}
+
 d3.select(self.frameElement).style("height", height + "px");
 
 function test(d) {
