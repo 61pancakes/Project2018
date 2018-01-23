743923367ddb0672cb5cf790f3c96995d0fcebc7
61pancakes <andrea@wcw-stud-145-109-58-209.wireless.uva.nl>
2018-01-23 14:09:41 +0100

    back to begin

--- data/javascript/sunburst.js
+++ data/javascript/sunburst.js
@@ -12,11 +12,35 @@ var margin = { top: 50, right: 250, bottom: 50, left: 50 },
     height = 600 - margin.top - margin.bottom,
     radius = (Math.min(width, height) / 2) - 10;
 
+// var x = d3.scale.linear()
+//     .range([0, 2 * Math.PI]);
+
+// var y = d3.scale.sqrt()
+//     .range([0, radius]);
+
+// var partition = d3.layout.partition()
+//     .value(function (d) { return d.size; });
+
+// var arc = d3.svg.arc()
+//     .startAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+//     .endAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+//     .innerRadius(function (d) { return Math.max(0, y(d.y)); })
+//     .outerRadius(function (d) { return Math.max(0, y(d.y + d.dy)); });
+
+// var svg = d3.select("body").append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//     .append("g")
+//     .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
 
 var y = d3.scale.sqrt()
     .range([0, radius]);
+
+var color = d3.scale.category20c();
 
 var partition = d3.layout.partition()
     .value(function (d) { return d.size; });
@@ -79,28 +103,24 @@ var colorSlice = function (d) {
     }
 }
 
-// function createSunburst() {
-/* Load dataset from local server to create the circle partitions. */
 d3.json("data/json/sunburst.json", function (error, root) {
+    if (error) throw error;
     var formatNumber = d3.format(",d");
+
+
     svg.selectAll("path")
         .data(partition.nodes(root))
         .enter().append("path")
         .attr("d", arc)
-        .style("fill", colorSlice)
-        .style("stroke-width", "0.5")
-        .on("click", updateSunburst)
+        .style("fill", function (d) { return color((d.children ? d : d.parent).name); })
+        .on("click", click)
         .append("title")
-        .text(function (d) { return d.name + "\nAantal studenten: " + formatNumber(d.value); });
+        .text(function (d) { return d.name + "\n" + formatNumber(d.value); });
 });
 
-d3.select(self.frameElement).style("height", height + "px");
-// };
-
-function updateSunburst(d) {
-    console.log(d);
-    var svg = d3.select("body").transition()
-        .duration(1000)
+function click(d) {
+    svg.transition()
+        .duration(750)
         .tween("scale", function () {
             var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
                 yd = d3.interpolate(y.domain(), [d.y, 1]),
@@ -111,4 +131,35 @@ function updateSunburst(d) {
         .attrTween("d", function (d) { return function () { return arc(d); }; });
 }
 
-// createSunburst();
+d3.select(self.frameElement).style("height", height + "px");
+
+
+// /* Load dataset from local server to create the circle partitions. */
+// d3.json("data/json/sunburst.json", function (error, root) {
+//     var formatNumber = d3.format(",d");
+//     svg.selectAll("path")
+//         .data(partition.nodes(root))
+//         .enter().append("path")
+//         .attr("d", arc)
+//         .style("fill", colorSlice)
+//         .style("stroke-width", "0.5")
+//         .on("click", updateSunburst)
+//         .append("title")
+//         .text(function (d) { return d.name + "\nAantal studenten: " + formatNumber(d.value); });
+// });
+
+// d3.select(self.frameElement).style("height", height + "px");
+
+// function updateSunburst(d) {
+//     console.log(d);
+//     var svg = d3.select("body").transition()
+//         .duration(1000)
+//         .tween("scale", function () {
+//             var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+//                 yd = d3.interpolate(y.domain(), [d.y, 1]),
+//                 yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+//             return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+//         })
+//         .selectAll("path")
+//         .attrTween("d", function (d) { return function () { return arc(d); }; });
+// }
