8c9ef28beb8c294969cd98a61b954758edc11f62
61pancakes <andrea@wcw-stud-145-109-58-209.wireless.uva.nl>
2018-01-23 14:14:26 +0100

    back to beginning 3

--- data/javascript/sunburst.js
+++ data/javascript/sunburst.js
@@ -12,34 +12,11 @@ var margin = { top: 50, right: 250, bottom: 50, left: 50 },
     height = 600 - margin.top - margin.bottom,
     radius = (Math.min(width, height) / 2) - 10;
 
-// var x = d3.scale.linear()
-//     .range([0, 2 * Math.PI]);
-
-// var y = d3.scale.sqrt()
-//     .range([0, radius]);
-
-// var partition = d3.layout.partition()
-//     .value(function (d) { return d.size; });
-
-// var arc = d3.svg.arc()
-//     .startAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-//     .endAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-//     .innerRadius(function (d) { return Math.max(0, y(d.y)); })
-//     .outerRadius(function (d) { return Math.max(0, y(d.y + d.dy)); });
-
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width)
-//     .attr("height", height)
-//     .append("g")
-//     .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
-
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
 
 var y = d3.scale.sqrt()
     .range([0, radius]);
-
-var color = d3.scale.category20c();
 
 var partition = d3.layout.partition()
     .value(function (d) { return d.size; });
