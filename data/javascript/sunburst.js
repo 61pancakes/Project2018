357ba97df8e5996ae81651899ae49beff2a6f4d3
61pancakes <andrea@wcw-stud-145-109-58-209.wireless.uva.nl>
2018-01-23 14:12:38 +0100

    back to beginning 2

--- data/javascript/sunburst.js
+++ data/javascript/sunburst.js
@@ -33,7 +33,6 @@ var margin = { top: 50, right: 250, bottom: 50, left: 50 },
 //     .append("g")
 //     .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
 
-
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
 
@@ -103,22 +102,22 @@ var colorSlice = function (d) {
     }
 }
 
+/* Load dataset from local server to create the circle partitions. */
 d3.json("data/json/sunburst.json", function (error, root) {
-    if (error) throw error;
     var formatNumber = d3.format(",d");
-
-
     svg.selectAll("path")
         .data(partition.nodes(root))
         .enter().append("path")
         .attr("d", arc)
-        .style("fill", function (d) { return color((d.children ? d : d.parent).name); })
+        .style("fill", colorSlice)
+        .style("stroke-width", "0.5")
         .on("click", click)
         .append("title")
-        .text(function (d) { return d.name + "\n" + formatNumber(d.value); });
+        .text(function (d) { return d.name + "\nAantal studenten: " + formatNumber(d.value); });
 });
 
 function click(d) {
+    console.log(d);
     svg.transition()
         .duration(750)
         .tween("scale", function () {
@@ -132,34 +131,3 @@ function click(d) {
 }
 
 d3.select(self.frameElement).style("height", height + "px");
-
-
-// /* Load dataset from local server to create the circle partitions. */
-// d3.json("data/json/sunburst.json", function (error, root) {
-//     var formatNumber = d3.format(",d");
-//     svg.selectAll("path")
-//         .data(partition.nodes(root))
-//         .enter().append("path")
-//         .attr("d", arc)
-//         .style("fill", colorSlice)
-//         .style("stroke-width", "0.5")
-//         .on("click", updateSunburst)
-//         .append("title")
-//         .text(function (d) { return d.name + "\nAantal studenten: " + formatNumber(d.value); });
-// });
-
-// d3.select(self.frameElement).style("height", height + "px");
-
-// function updateSunburst(d) {
-//     console.log(d);
-//     var svg = d3.select("body").transition()
-//         .duration(1000)
-//         .tween("scale", function () {
-//             var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-//                 yd = d3.interpolate(y.domain(), [d.y, 1]),
-//                 yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
-//             return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
-//         })
-//         .selectAll("path")
-//         .attrTween("d", function (d) { return function () { return arc(d); }; });
-// }
