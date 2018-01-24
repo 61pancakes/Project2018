0a33a434599c56bdc3cbf24ce1d01a57a3882b4e
61pancakes <andrea@wcw-stud-145-109-43-73.wireless.uva.nl>
2018-01-24 10:10:06 +0100

    interactivity line graph

--- data/javascript/linegraph.js
+++ data/javascript/linegraph.js
@@ -126,7 +126,7 @@
                 .style("stroke", colors[i])
                 .style("stroke-dasharray", ("5, 5"))
                 .attr("d", lineF(finalData[i]))
-                .attr("id", "a")
+                .attr("id", legendData[i].color)
                 .on("mouseover", function (d) {
                     console.log(this);
                     d3.selectAll("#" + this.id).style("stroke-width", "5")
@@ -140,7 +140,7 @@
                 .attr("class", "line")
                 .style("stroke", colors[i])
                 .attr("d", lineM(finalData[i]))
-                .attr("id", "a")
+                .attr("id", legendData[i].color)
                 .on("mouseover", function (d) {
                     d3.selectAll("#" + this.id).style("stroke-width", "5")
                 })
