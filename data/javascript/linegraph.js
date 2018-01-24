3d9f5110f161996d0dfc53a5ec3e5c8f9343c4ea
61pancakes <andrea@wcw-stud-145-109-43-73.wireless.uva.nl>
2018-01-24 11:09:01 +0100

    line graph interactivity

--- data/javascript/linegraph.js
+++ data/javascript/linegraph.js
@@ -130,11 +130,11 @@
                 .attr("d", lineF(finalData[i]))
                 .attr("id", ids[i])
                 .on("mouseover", function (d) {
-                    console.log(d3.selectAll(this.id));
-                    d3.selectAll(this.id).style("stroke-width", "5")
+                    console.log("THIS" + d3.select(this));
+                    d3.select("path").style("stroke-width", "5")
                 })
                 .on("mouseout", function (d) {
-                    d3.selectAll(this.id).style("stroke-width", "2")
+                    d3.select("path").style("stroke-width", "2")
                 });
             // .on("click", onclick(i));
 
@@ -144,11 +144,11 @@
                 .attr("d", lineM(finalData[i]))
                 .attr("id", ids[i])
                 .on("mouseover", function (d) {
-                    console.log(d3.selectAll(this.id));
-                    d3.selectAll(this.id).style("stroke-width", "5")
+                    console.log("THIS ID" + d3.select(this.id));
+                    d3.select("path").style("stroke-width", "5")
                 })
                 .on("mouseout", function (d) {
-                    d3.selectAll(this.id).style("stroke-width", "2")
+                    d3.select("path").style("stroke-width", "2")
                 });
             // .on("click", onclick());
         }
